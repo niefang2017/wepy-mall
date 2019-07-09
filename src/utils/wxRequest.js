3295479aa08dbd4edfa986557b134d43ecea5a38
聂芳 <nie4161204@qq.com>
2019-07-10 01:37:58 +0800

    注释全局loading

--- src/utils/wxRequest.js
+++ src/utils/wxRequest.js
@@ -13,7 +13,7 @@ const codeMessage = {
   504: '网关超时。'
 };
 const wxRequest = async (params = {}, url) => {
-  tip.loading();
+  // tip.loading();
   wx.showNavigationBarLoading();
   let store = getStore();
   // let loginInfo = store.getState().user.loginInfo;
@@ -39,7 +39,7 @@ const wxRequest = async (params = {}, url) => {
       'clientType': 'wechat'
     }
   });
-  tip.loaded();
+  // tip.loaded();
   wx.hideNavigationBarLoading();
   if (res.statusCode === 200) {
     return res.data;
